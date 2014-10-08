b3b959ddb8fd3a8731184ae31f600f45253f2e2c
Sandro Santilli <strk@keybit.net>
2014-10-08 13:47:45 +0200

    Add dev user creation and /etc/hosts editing

--- start.sh
+++ start.sh
@@ -2,9 +2,26 @@
 
 export RAILS_ENV=development
 
+dev=`psql -Atc "select username from users where username = 'dev'" carto_db_development`
+test $? -gt 0 && exit 1
+if test -z "$dev"; then
+  echo "No 'dev' user exists, creating one"
+  cd rails-app/script/
+  ./create_dev_user dev dev dev ${USER}@localhost
+  cd -
+fi
+
+grep -q dev\.localhost\.lan /etc/hosts
+if test $? -gt 0; then
+  echo "Adding 'dev.localhost.lan' to /etc/hosts"
+  sudo sed -i~ 's/\(^127.0.0.1.*\)/\1 dev.localhost.lan/' /etc/hosts
+  #sudo cp /etc/hosts /etc/hosts~
+fi
 
 #sudo varnishd -a localhost:6081 -f varnish-default.vcl -T localhost:6082
 
+
+# TODO: check that the app was not started already
 {
 cd rails-app
 #redis-server > /tmp/redis.log &
@@ -14,14 +31,18 @@ rails s > /tmp/rails.log &
 cd -
 }
 
+# TODO: check that the app was not started already
 {
 cd map-api
 node app.js development : tiler > /tmp/tiler.log 2>&1 &
 cd -
 }
 
+# TODO: check that the app was not started already
 {
 cd sql-api
 node app.js development : sqlapi > /tmp/sqlapi.log 2>&1 &
 cd -
 }
+
+echo "Service should be ready in a moment on http://dev.localhost.lan:3000"
